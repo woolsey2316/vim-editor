500d15cb80651b16bbdbd2c13d5cb6a6bd8a6a66
David <woolsey2316@gmail.com>
2025-03-28 15:26:45 +1100

    Home and end keys

--- kilo.c
+++ kilo.c
@@ -18,13 +18,15 @@
 enum editorKey {
   ARROW_LEFT = 1000,
   ARROW_RIGHT,
-  ARROW_UP, 
-  ARROW_DOWN, 
+  ARROW_UP,
+  ARROW_DOWN,
+  DEL_KEY,
   HOME_KEY,
   END_KEY,
   PAGE_UP,
-  PAGE_DOWN,
-}
+  PAGE_DOWN
+};
+
 /*** data ***/
 
 struct editorConfig {
@@ -80,14 +82,15 @@ int editorReadKey() {
     if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
 
     if (seq[0] == '[') {
-      if (seq[1] > '0' && seq[1] <= '9') {
-        if (read(STDIN_FILENO, &seq[2], 1) != ) return '\x1b';
+      if (seq[1] >= '0' && seq[1] <= '9') {
+        if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
         if (seq[2] == '~') {
           switch (seq[1]) {
             case '1': return HOME_KEY;
+            case '3': return DEL_KEY;
             case '4': return END_KEY;
-            case '5': return PAGE_DOWN;
-            case '6': return PAGE_UP;
+            case '5': return PAGE_UP;
+            case '6': return PAGE_DOWN;
             case '7': return HOME_KEY;
             case '8': return END_KEY;
           }
@@ -102,13 +105,13 @@ int editorReadKey() {
           case 'F': return END_KEY;
         }
       }
-    } else if (seq[0] == 0) {
-        switch(seq[1]) {
-          case 'H': return HOME_KEY;
-          case 'F': return END_KEY;
+    } else if (seq[0] == 'O') {
+      switch (seq[1]) {
+        case 'H': return HOME_KEY;
+        case 'F': return END_KEY;
       }
     }
-    
+
     return '\x1b';
   } else {
     return c;
@@ -219,12 +222,12 @@ void editorRefreshScreen() {
 
 void editorMoveCursor(int key) {
   switch (key) {
-    case ARROW_LEFT: 
+    case ARROW_LEFT:
       if (E.cx != 0) {
         E.cx--;
       }
       break;
-    case ARROW_RIGHT: 
+    case ARROW_RIGHT:
       if (E.cx != E.screencols - 1) {
         E.cx++;
       }
@@ -251,20 +254,24 @@ void editorProcessKeypress() {
       write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
       break;
+
     case HOME_KEY:
       E.cx = 0;
       break;
+
     case END_KEY:
       E.cx = E.screencols - 1;
       break;
+
     case PAGE_UP:
     case PAGE_DOWN:
       {
         int times = E.screenrows;
         while (times--)
-          editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN;
+          editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
       }
       break;
+
     case ARROW_UP:
     case ARROW_DOWN:
     case ARROW_LEFT:
