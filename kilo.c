2306e2051029844d82bb3a86722211d13257e0a4
David <woolsey2316@gmail.com>
2025-03-28 11:19:34 +1100

    welcome message

--- kilo.c
+++ kilo.c
@@ -1,15 +1,17 @@
 /*** includes ***/
 
 #include <ctype.h>
-#include <string.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
 /*** defines ***/
+
+#define KILO_VERSION "0.0.1"
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
@@ -95,31 +97,47 @@ int getWindowSize(int *rows, int *cols) {
 }
 
 /*** append buffer ***/
+
 struct abuf {
   char *b;
   int len;
 };
+
 #define ABUF_INIT {NULL, 0}
 
 void abAppend(struct abuf *ab, const char *s, int len) {
   char *new = realloc(ab->b, ab->len + len);
 
   if (new == NULL) return;
-  memcpy(&new[ab->len],s, len);
+  memcpy(&new[ab->len], s, len);
   ab->b = new;
   ab->len += len;
-  
 }
 
 void abFree(struct abuf *ab) {
   free(ab->b);
 }
+
 /*** output ***/
 
 void editorDrawRows(struct abuf *ab) {
   int y;
   for (y = 0; y < E.screenrows; y++) {
-    abAppend(ab, "~", 1);
+    if (y == E.screenrows / 3) {
+      char welcome[80];
+      int welcomelen = snprintf(welcome, sizeof(welcome),
+        "Kilo editor -- version %s", KILO_VERSION);
+      if (welcomelen > E.screencols) welcomelen = E.screencols;
+      int padding = (E.screencols - welcomelen) / 2;
+      if (padding) {
+        abAppend(ab, "~", 1);
+        padding--;
+      }
+      while (padding--) abAppend(ab, " ", 1);
+      abAppend(ab, welcome, welcomelen);
+    } else {
+      abAppend(ab, "~", 1);
+    }
 
     abAppend(ab, "\x1b[K", 3);
     if (y < E.screenrows - 1) {
@@ -142,6 +160,7 @@ void editorRefreshScreen() {
   write(STDOUT_FILENO, ab.b, ab.len);
   abFree(&ab);
 }
+
 /*** input ***/
 
 void editorProcessKeypress() {
